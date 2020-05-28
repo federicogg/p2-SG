fa42cb5c24aff28ddbbc2a9c49f3ba32e48311d2
Fede <fedeinformatica9@gmail.com>
2020-05-28 18:24:18 +0200

    commit

--- src/Salida.js
+++ src/Salida.js
@@ -1,20 +1,20 @@
-class Salida extends THREE.Object3D{
-    constructor(){
+class Salida extends THREE.Object3D {
+    constructor() {
         super();
-        var boxgeometry = new THREE.CylinderGeometry(3,3,1,64);
-        var material = new THREE.MeshPhongMaterial({ color: 0x0033ff});
+        var boxgeometry = new THREE.CylinderGeometry(3, 3, 1, 64);
+        var material = new THREE.MeshPhongMaterial({ color: 0x0033ff });
         var caja1 = new THREE.Mesh(boxgeometry, material);
-        var caja2 =new THREE.Mesh(boxgeometry, material);;
-        var torogeometry = new THREE.TorusGeometry( 20, 3, 16, 100,3.1 );
-        var arco = new THREE.Mesh(torogeometry,material);
+        var caja2 = new THREE.Mesh(boxgeometry, material);;
+        var torogeometry = new THREE.TorusGeometry(20, 3, 16, 100, 3.1);
+        var arco = new THREE.Mesh(torogeometry, material);
 
         caja1.scale.y = 50;
-       
+
         caja1.position.x = -70;
         caja1.position.z = 20;
 
         caja2.scale.y = 50;
-       
+
         caja2.position.x = -70;
         caja2.position.z = -20;
 
